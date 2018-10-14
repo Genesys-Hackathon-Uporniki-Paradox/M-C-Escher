c7ca223592e48d356b9e3c1e39a48144d060ddbf
Antony Bogun <antonybogun@gmail.com>
2018-10-13 21:56:24 -0400

    Add service-client integration

--- scripts/main.js
+++ scripts/main.js
@@ -2,3 +2,33 @@
  *  JS goes here
  */
 
+function initializeWebPage() {
+    genesys.wwe.service.subscribe(["agent", "interaction", "media", "system", "markdone"], eventHandler, this);
+}
+
+function eventHandler(message) {
+    switch (message.event) {
+        case "agent":
+            log("Received agent event: " + JSON.stringify(message, null, "\t"));
+            break;
+        case "interaction":
+            log("Received interaction event: " + JSON.stringify(message, null, "\t"));
+            break;
+        case "markdone":
+            log("Received markdone event: " + JSON.stringify(message, null, "\t"));
+            break;
+        case "media":
+            log("Received media event: " + JSON.stringify(message, null, "\t"));
+            break;
+        case "system":
+            log("Received system event: " + JSON.stringify(message, null, "\t"));
+            break;
+
+        default:
+    }
+}
+
+function log(text) {
+    var divOutput = document.getElementById("output");
+    divOutput.innerHTML = "<pre>" + text + "</pre><br/>" + divOutput.innerHTML;
+}
